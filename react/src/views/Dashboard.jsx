dff05156f34e07da3da07aa697ab09be947a624c
SebastianParejaJimenez <110253953+SebastianParejaJimenez@users.noreply.github.com>
2023-07-23 18:59:41 -0500

    Initial commit

---
+++ react/src/views/Dashboard.jsx
@@ -0,0 +1,11 @@
+import PageComponent from "../components/PageComponent";
+
+export default function Dashboard() {
+  return (
+    <>
+        <PageComponent title="Dashboard">
+            Children
+        </PageComponent>
+    </>
+  )
+}
